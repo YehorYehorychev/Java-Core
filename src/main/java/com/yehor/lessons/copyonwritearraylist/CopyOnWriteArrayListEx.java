d72aba5dbe398df5d04c228517ee25a57f4cd3b9
YehorYehorychev <egor57899@gmail.com>
2024-03-08 18:13:07 -0800

    CopyOnWriteArrayList 'Before' example added

--- src/main/java/com/yehor/lessons/copyonwritearraylist/CopyOnWriteArrayListEx.java
+++ src/main/java/com/yehor/lessons/copyonwritearraylist/CopyOnWriteArrayListEx.java
@@ -1,8 +1,46 @@
 package com.yehor.lessons.copyonwritearraylist;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class CopyOnWriteArrayListEx {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Yehor");
+        list.add("Mike");
+        list.add("Phill");
+        list.add("John");
+        list.add("David");
+        System.out.println(list);
 
+        Runnable runnable1 = () -> {
+            Iterator<String> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(iterator.next());
+            }
+        };
+
+        Runnable runnable2 = () -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list.remove(4);
+            list.add("Elena");
+        };
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(list);
     }
 }
