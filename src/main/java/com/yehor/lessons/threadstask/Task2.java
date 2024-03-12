5c1bb5527ea464bcb988739638092cd56a6ca6d4
YehorYehorychev <egor57899@gmail.com>
2024-03-11 19:21:10 -0700

    Task2 class added

---
+++ src/main/java/com/yehor/lessons/threadstask/Task2.java
@@ -0,0 +1,23 @@
+package com.yehor.lessons.threadstask;
+
+/**
+ * Russian:
+ * 1.  Задан массив случайных целых чисел (от 1 до 300)
+ * случайной длины (до 1 млн элементов).
+ * Найти максимальный элемент в массиве двумя способами: в
+ * одном потоке и используя 10 потоков.
+ * Сравнить затраченное в обоих случаях время.
+ * ##################################################################
+ * English:
+ * Given an array of random integers (from 1 to 300) of random length
+ * (up to 1 million elements). Find the maximum element in the array
+ * using two methods: in one thread and using 10 threads.
+ * Compare the time spent in both cases.
+ */
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+    }
+}
