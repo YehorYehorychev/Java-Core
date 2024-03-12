afa59d991b069d6d882f90aeeaefd8cbe1a075b1
YehorYehorychev <egor57899@gmail.com>
2024-03-11 19:45:58 -0700

    Task2 final iteration added

--- src/main/java/com/yehor/lessons/threadstask/Task2.java
+++ src/main/java/com/yehor/lessons/threadstask/Task2.java
@@ -1,6 +1,9 @@
 package com.yehor.lessons.threadstask;
 
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
@@ -22,14 +25,23 @@ import java.util.stream.IntStream;
 public class Task2 {
 
     public static void main(String[] args) {
-        int[] values = new int[1_000_000];
+        int[] values = new int[100_000_000];
         Random random = new Random();
         for (int i = 0; i < values.length; i++) {
             values[i] = random.nextInt(300) + 1;
         }
 
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
         trackTime(() -> findMax(values));
-        trackTime(() -> findMaxParallel(values));
+        trackTime(() -> {
+            try {
+                return findMaxParallel(values, threadPool);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Integer.MIN_VALUE;
+        });
+        threadPool.shutdownNow();
     }
 
     private static int trackTime(Supplier<Integer> task) {
@@ -40,15 +52,17 @@ public class Task2 {
     }
 
     private static int findMax(int[] values) {
+        System.out.println("Single");
         return IntStream.of(values)
                 .max()
                 .orElse(Integer.MIN_VALUE);
     }
 
-    private static int findMaxParallel(int[] values) {
-        return IntStream.of(values)
+    private static int findMaxParallel(int[] values, ExecutorService executorService) throws ExecutionException, InterruptedException {
+        System.out.println("Parallel");
+        return executorService.submit(() -> IntStream.of(values)
                 .parallel()
                 .max()
-                .orElse(Integer.MIN_VALUE);
+                .orElse(Integer.MIN_VALUE)).get();
     }
 }
