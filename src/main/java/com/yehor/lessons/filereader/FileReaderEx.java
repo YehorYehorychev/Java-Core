a7e76539ee7eeeb8d72763928abb63ce902516d8
YehorYehorychev <egor57899@gmail.com>
2024-03-12 19:07:34 -0700

    FileReader first iteration added

--- src/main/java/com/yehor/lessons/filereader/FileReaderEx.java
+++ src/main/java/com/yehor/lessons/filereader/FileReaderEx.java
@@ -1,8 +1,25 @@
 package com.yehor.lessons.filereader;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileReaderEx {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        FileReader reader = null;
+        try {
+            reader = new FileReader("C:\\Users\\egory\\IdeaProjects\\JavaLessons\\files\\rubai.txt");
+            int character;
+            while ((character = reader.read()) != -1) {
+                System.out.println((char) character);
+            }
+            System.out.println();
+            System.out.println("Done");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
+        }
     }
 }
