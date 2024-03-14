c2f4a887b688569902ceb1780c25e27bb6853257
YehorYehorychev <egor57899@gmail.com>
2024-03-13 18:52:24 -0700

    BufferedReader/Wrighter second iteration added

--- src/main/java/com/yehor/lessons/bufferedwriterreader/CopyEx.java
+++ src/main/java/com/yehor/lessons/bufferedwriterreader/CopyEx.java
@@ -7,10 +7,16 @@ public class CopyEx {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new FileReader("files/rubai.txt"));
              BufferedWriter writer = new BufferedWriter(new FileWriter("files/rubai2.txt"))) {
-            int character;
-            while ((character = reader.read()) != -1) {
-                writer.write(character);
+//            int character;
+//            while ((character = reader.read()) != -1) {
+//                writer.write(character);
+//            }
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.write('\n');
             }
+            System.out.println("Done!");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
