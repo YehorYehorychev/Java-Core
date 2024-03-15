5a1adf291e719ac5da59494ce7ed3bf7fa0defa0
YehorYehorychev <egor57899@gmail.com>
2024-03-14 19:24:05 -0700

    Serialization final iteration of functionality added

--- src/main/java/com/yehor/lessons/serialization/programmer1/SerializationEx2.java
+++ src/main/java/com/yehor/lessons/serialization/programmer1/SerializationEx2.java
@@ -9,7 +9,7 @@ public class SerializationEx2 {
 
     public static void main(String[] args) {
         Car car = new Car("Chevy Bolt EV", "Black");
-        Employees employees = new Employees("Maria", "IT", 28, 6.200, car);
+        Employees employees = new Employees("Maria", "Brooks","IT", 6.200, car);
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("files/employees2.bin"))) {
             outputStream.writeObject(employees);
             System.out.println("Done!");
