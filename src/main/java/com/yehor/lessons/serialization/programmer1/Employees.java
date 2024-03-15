ce6c9d7cd3739d73e777e289273a2c6b14ae4dec
YehorYehorychev <egor57899@gmail.com>
2024-03-14 18:25:08 -0700

    Serialization third iteration of functionality added

---
+++ src/main/java/com/yehor/lessons/serialization/programmer1/Employees.java
@@ -0,0 +1,25 @@
+package com.yehor.lessons.serialization.programmer1;
+
+public class Employees {
+    String name;
+    String department;
+    int age;
+    double salary;
+
+    public Employees(String name, String department, int age, double salary) {
+        this.name = name;
+        this.department = department;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employees{" +
+                "name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/main/java/com/yehor/lessons/serialization/programmer1/SerializationEx2.java
@@ -0,0 +1,8 @@
+package com.yehor.lessons.serialization.programmer1;
+
+public class SerializationEx2 {
+
+    public static void main(String[] args) {
+
+    }
+}
