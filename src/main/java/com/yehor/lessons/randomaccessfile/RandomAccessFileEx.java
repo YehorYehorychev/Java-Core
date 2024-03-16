fab431317124a6cf241778dc5016ec2bb4f18835
YehorYehorychev <egor57899@gmail.com>
2024-03-15 18:07:45 -0700

    RandomAccessFile functions first iteration added

---
+++ src/main/java/com/yehor/lessons/randomaccessfile/RandomAccessFileEx.java
@@ -0,0 +1,19 @@
+package com.yehor.lessons.randomaccessfile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileEx {
+
+    public static void main(String[] args) {
+        try (RandomAccessFile file = new RandomAccessFile("files/test10.txt", "rw")) {
+            int a = file.read();
+            System.out.println((char) a);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
