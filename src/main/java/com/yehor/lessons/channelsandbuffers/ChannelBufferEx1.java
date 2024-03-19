830f212000b82bd221fef9c2a8fa3ab6def72756
YehorYehorychev <egor57899@gmail.com>
2024-03-18 19:02:18 -0700

    Channel and Buffer methods and main functionality demo added(fifth iteration)

--- src/main/java/com/yehor/lessons/channelsandbuffers/ChannelBufferEx1.java
+++ src/main/java/com/yehor/lessons/channelsandbuffers/ChannelBufferEx1.java
@@ -29,9 +29,11 @@ public class ChannelBufferEx1 {
             String text = "\n His palms are sweaty, knees weak, arms are heavy\n" +
                     "There's vomit on his sweater already, mom's spaghetti\n" +
                     "He's nervous, but on the surface, he looks calm and ready\n";
-            ByteBuffer buffer2 = ByteBuffer.allocate(text.getBytes().length);
-            buffer2.put(text.getBytes());
-            buffer2.flip();
+//            ByteBuffer buffer2 = ByteBuffer.allocate(text.getBytes().length);
+//            buffer2.put(text.getBytes());
+//            buffer2.flip();
+//            channel.write(buffer2);
+            ByteBuffer buffer2 = ByteBuffer.wrap(text.getBytes());
             channel.write(buffer2);
 
         } catch (FileNotFoundException e) {
