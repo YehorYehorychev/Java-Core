4c88542dc8476b1030b17e6872c02c0ec2ee5839
YehorYehorychev <egor57899@gmail.com>
2024-03-18 19:15:34 -0700

    Channel and Buffer methods first iteration added

---
+++ src/main/java/com/yehor/lessons/channelsandbuffers/ChannelBufferEx2.java
@@ -0,0 +1,28 @@
+package com.yehor.lessons.channelsandbuffers;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class ChannelBufferEx2 {
+
+    public static void main(String[] args) {
+        try (RandomAccessFile file = new RandomAccessFile("files/test5.txt", "r");
+             FileChannel channel = file.getChannel()) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(5);
+            channel.read(buffer);
+            buffer.flip();
+            System.out.println((char) buffer.get());
+            System.out.println((char) buffer.get());
+            System.out.println((char) buffer.get());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
