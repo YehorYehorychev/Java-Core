f52b8e19ee229c9e899613cd8e7d6060a22e8a7f
YehorYehorychev <egor57899@gmail.com>
2024-03-19 18:22:27 -0700

    Path/Files class created

---
+++ src/main/java/com/yehor/lessons/path/PathAndFilesEx1.java
@@ -0,0 +1,11 @@
+package com.yehor.lessons.path;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathAndFilesEx1 {
+
+    public static void main(String[] args) {
+        Path filePath = Paths.get("files/test15.txt");
+    }
+}
