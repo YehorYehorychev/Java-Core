a4b2a117f75f1a81e909559b63658fd2b67f3903
YehorYehorychev <egor57899@gmail.com>
2024-03-19 18:25:00 -0700

    Path/Files class basic methods created

--- src/main/java/com/yehor/lessons/path/PathAndFilesEx1.java
+++ src/main/java/com/yehor/lessons/path/PathAndFilesEx1.java
@@ -7,5 +7,10 @@ public class PathAndFilesEx1 {
 
     public static void main(String[] args) {
         Path filePath = Paths.get("files/test15.txt");
+        Path directoryPath = Paths.get("C:/Users/egory/IdeaProjects/JavaLessons/files");
+
+        System.out.println("filePath.getFileName(): " + filePath.getFileName());
+        System.out.println("directoryPath.getFileName(): " + directoryPath.getFileName());
+        System.out.println("----------------------------------------");
     }
 }
