20f6dc89afc7f6aaac8bdfb7b737c509a0c73697
YehorYehorychev <egor57899@gmail.com>
2024-03-20 18:37:27 -0700

    Path/Files second class with additional methods added (FINAL iteration)

--- src/main/java/com/yehor/lessons/path/PathAndFilesEx3.java
+++ src/main/java/com/yehor/lessons/path/PathAndFilesEx3.java
@@ -6,13 +6,19 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class PathAndFilesEx3 {
 
     public static void main(String[] args) throws IOException {
         Path filePath = Paths.get("files/test10.txt");
-        Files.createFile(filePath);
-        String dialog = "-Hello\n-Hello\n-How are you?\nI'm good, you?\nI'm good, thanks!";
-        Files.write(filePath, dialog.getBytes(StandardCharsets.UTF_8));
+//        Files.createFile(filePath);
+//        String dialog = "-Hello\n-Hello\n-How are you?\nI'm good, you?\nI'm good, thanks!";
+//        Files.write(filePath, dialog.getBytes(StandardCharsets.UTF_8));
+
+        List<String> list = Files.readAllLines(filePath);
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
