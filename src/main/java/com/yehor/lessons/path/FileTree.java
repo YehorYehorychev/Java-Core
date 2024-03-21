028da18466ecc239ab8987a5949916e735683618
YehorYehorychev <egor57899@gmail.com>
2024-03-20 18:52:50 -0700

    WalkFileTree methods added (First iteration)

---
+++ src/main/java/com/yehor/lessons/path/FileTree.java
@@ -0,0 +1,42 @@
+package com.yehor.lessons.path;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileTree {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class MyFileVisitor implements FileVisitor<Path> {
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println("Enter to dir:" + dir);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println("File name: " + file.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error while visiting file: " + file.getFileName());
+        return FileVisitResult.TERMINATE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("Exit from dir: " + dir);
+        return FileVisitResult.CONTINUE;
+    }
+    }
+}
