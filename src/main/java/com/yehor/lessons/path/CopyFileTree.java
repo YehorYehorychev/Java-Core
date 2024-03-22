dbc1018fe005a4f94bf818b7a05314bda7560670
YehorYehorychev <egor57899@gmail.com>
2024-03-21 18:01:08 -0700

    WalkFileTree methods added (Copy folder with files functionality added)

--- src/main/java/com/yehor/lessons/path/CopyFileTree.java
+++ src/main/java/com/yehor/lessons/path/CopyFileTree.java
@@ -6,8 +6,11 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class CopyFileTree {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Path source = Paths.get("C:/Users/egory/IdeaProjects/JavaLessons/files/Folder");
+        Path destination = Paths.get("C:/Users/egory/IdeaProjects/JavaLessons/files/Copy");
+        Files.walkFileTree(source, new MyFileVisitor2(source, destination));
+        System.out.println("Done!");
     }
 }
 
@@ -23,14 +26,14 @@ class MyFileVisitor2 extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
         Path newDestination = desatination.resolve(source.relativize(dir));
-        Files.copy(dir, newDestination);
+        Files.copy(dir, newDestination, StandardCopyOption.REPLACE_EXISTING);
         return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         Path newDestination = desatination.resolve(source.relativize(file));
-        Files.copy(file, newDestination);
+        Files.copy(file, newDestination, StandardCopyOption.REPLACE_EXISTING);
         return FileVisitResult.CONTINUE;
     }
 }
