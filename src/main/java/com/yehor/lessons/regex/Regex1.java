6af7a384fb65fcbfde426dfeb57d723eca5d1c11
YehorYehorychev <egor57899@gmail.com>
2024-03-22 16:08:16 -0700

    RegEx1 class and first template added

---
+++ src/main/java/com/yehor/lessons/regex/Regex1.java
@@ -0,0 +1,11 @@
+package com.yehor.lessons.regex;
+
+public class Regex1 {
+
+    public static void main(String[] args) {
+        String s = "Yehor Yehorychev, USA, Mountain View, California ST, 2342, apt 10" +
+                "email: egor@email.com, ZIP: 94040, CellPhone: +189485856" +
+                "Aaron Paul, USA, New-York, 50th ave ST, 5011, apt 34" +
+                "email: ddffw@email.com, ZIP: 45255 CellPhone: +184535669";
+    }
+}
