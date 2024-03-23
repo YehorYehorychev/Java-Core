9b909394773966b3c271ba096d93984246611561
YehorYehorychev <egor57899@gmail.com>
2024-03-22 17:51:28 -0700

    RegEx2 class and second pattern added

--- src/main/java/com/yehor/lessons/regex/Regex1.java
+++ src/main/java/com/yehor/lessons/regex/Regex1.java
@@ -1,5 +1,8 @@
 package com.yehor.lessons.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex1 {
 
     public static void main(String[] args) {
@@ -7,5 +10,12 @@ public class Regex1 {
                 "email: egor@email.com, ZIP: 94040, CellPhone: +189485856" +
                 "Aaron Paul, USA, New-York, 50th ave ST, 5011, apt 34" +
                 "email: ddffw@email.com, ZIP: 45255 CellPhone: +184535669";
+
+        Pattern pattern = Pattern.compile("email:");
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
     }
 }
--- src/main/java/com/yehor/lessons/regex/Regex2.java
+++ src/main/java/com/yehor/lessons/regex/Regex2.java
@@ -6,12 +6,22 @@ import java.util.regex.Pattern;
 public class Regex2 {
 
     public static void main(String[] args) {
-        String s1 = "ABDOP";
-        Pattern pattern1 = Pattern.compile("AB[C-F]OP");
-        Matcher matcher = pattern1.matcher(s1);
-
-        while (matcher.find()) {
-            System.out.println( matcher.group() + " " + "Position: " + matcher.start());
-        }
+//        String s1 = "ABDOP";
+//        Pattern pattern1 = Pattern.compile("AB[C-F]OP");
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc(e|5)");
+//        Matcher matcher = pattern1.matcher(s1);
+//
+//
+//        while (matcher.find()) {
+//            System.out.println( matcher.group() + " " + "Position: " + matcher.start());
+//        }
+        String phoneNubmer = "+375 (29) 898-12-13";
+        String regex = "(\\+375 \\(\\d{2}\\) \\d{3}-\\d{2}-\\d{2})";
+        Pattern pattern2 = Pattern.compile(regex);
+        Matcher matcher2 = pattern2.matcher(phoneNubmer);
+        System.out.println(matcher2.matches());
+        System.out.println(Pattern.matches(regex, phoneNubmer));
+        System.out.println(phoneNubmer.matches(regex));
     }
 }
