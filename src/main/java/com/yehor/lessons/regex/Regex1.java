b1989d443bb0521536c117279e0c8b04982f1310
YehorYehorychev <egor57899@gmail.com>
2024-03-25 18:39:46 -0700

    Class Regex1 first functionality added (Second iteration)

--- src/main/java/com/yehor/lessons/regex/Regex1.java
+++ src/main/java/com/yehor/lessons/regex/Regex1.java
@@ -6,9 +6,9 @@ import java.util.regex.Pattern;
 public class Regex1 {
 
     public static void main(String[] args) {
-        String s = "Yehor Yehorychev, USA, Mountain View, California ST, 2342, apt 10" +
+        String s = "Yehor Yehorychev, USA, Mountain View, California ST, 2342, apt 10 " +
                 "email: egor@email.com, ZIP: 94040, CellPhone: +189485856" +
-                "Aaron Paul, USA, New-York, 50th ave ST, 5011, apt 34" +
+                "Aaron Paul, USA, New-York, 50th ave ST, 5011, apt 34 " +
                 "email: ddffw@email.com, ZIP: 45255 CellPhone: +184535669";
 
 //        Pattern pattern = Pattern.compile("email:");
@@ -17,7 +17,8 @@ public class Regex1 {
 //        while (matcher.find()) {
 //            System.out.println(matcher.group());
 //        }
-        Pattern pattern = Pattern.compile("\\w+");
+//        Pattern pattern = Pattern.compile("\\b\\d{2}\\b");
+        Pattern pattern = Pattern.compile("\\+\\d{9}");
         Matcher matcher = pattern.matcher(s);
         while (matcher.find()) {
             System.out.println(matcher.group());
