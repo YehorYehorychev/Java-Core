b0d5b6618606d70582046c4027c269ac7116f43b
YehorYehorychev <egor57899@gmail.com>
2024-03-26 18:29:36 -0700

    Class Regex 4 replace methods added

--- src/main/java/com/yehor/lessons/regex/Regex4.java
+++ src/main/java/com/yehor/lessons/regex/Regex4.java
@@ -3,9 +3,14 @@ package com.yehor.lessons.regex;
 public class Regex4 {
 
     public static void main(String[] args) {
-        String s1 = "Hello my friend!, how are you?";
+        String s1 = "Hello   my    friend!,    how are you?";
         System.out.println(s1);
         s1 = s1.replace("friend", "buddy");
+        System.out.println(s1);
+        s1 = s1.replaceAll(" {2,}", " ");
+        s1 = s1.replaceAll("\\bh\\w+", "4444");
+        System.out.println(s1);
+        s1 = s1.replaceFirst("\\bH\\w+", "4444");
         System.out.println(s1);
     }
 }
