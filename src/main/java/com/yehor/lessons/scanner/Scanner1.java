849176b5fce6a2c9242974ee42e2b83b78faad67
YehorYehorychev <egor57899@gmail.com>
2024-03-27 17:43:21 -0700

    Class Scanner additional methods added second iteration

--- src/main/java/com/yehor/lessons/scanner/Scanner1.java
+++ src/main/java/com/yehor/lessons/scanner/Scanner1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Scanner1 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
 //        System.out.println("Type a numer: ");
 //        int i = scanner.nextInt();
 //        System.out.println("The number is: " + i);
@@ -14,8 +14,19 @@ public class Scanner1 {
 //        int y = scanner.nextInt();
 //        System.out.println(x/y);
 //        System.out.println(x%y);
-        System.out.println("Type a couple of words here: ");
-        String s = scanner.nextLine();
-        System.out.println("You typed: " + s);
+//        System.out.println("Type a couple of words here: ");
+//        String s = scanner.nextLine();
+//        String s2 = scanner.nextLine();
+//        System.out.println("You typed: " + s + " | " + s2);
+//        System.out.println("Type a numer: ");
+//        double i = scanner.nextDouble();
+//        System.out.println("The number is: " + i);
+        Scanner scanner = new Scanner("Hello my friend!\nHow are you?");
+//        String s = scanner.nextLine();
+//        System.out.println(s);
+//        System.out.println(scanner.nextLine());
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
     }
 }
