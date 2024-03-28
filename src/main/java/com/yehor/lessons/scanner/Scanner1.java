a73ff050dcd19a24a2f23036cfa1886416bcd518
YehorYehorychev <egor57899@gmail.com>
2024-03-27 17:36:08 -0700

    Class Scanner additional methods added

--- src/main/java/com/yehor/lessons/scanner/Scanner1.java
+++ src/main/java/com/yehor/lessons/scanner/Scanner1.java
@@ -6,8 +6,16 @@ public class Scanner1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Type a numer: ");
-        int i = scanner.nextInt();
-        System.out.println("The number is: " + i);
+//        System.out.println("Type a numer: ");
+//        int i = scanner.nextInt();
+//        System.out.println("The number is: " + i);
+//        System.out.println("Type two numbers here: ");
+//        int x = scanner.nextInt();
+//        int y = scanner.nextInt();
+//        System.out.println(x/y);
+//        System.out.println(x%y);
+        System.out.println("Type a couple of words here: ");
+        String s = scanner.nextLine();
+        System.out.println("You typed: " + s);
     }
 }
