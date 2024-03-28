c3106381479c5f9f78f1bf1e8b5e0b48b01872aa
YehorYehorychev <egor57899@gmail.com>
2024-03-27 18:11:22 -0700

    Class Person,User,ReflectionAPIEx created for the future ReflictionAPI functionality demo (second iteration)

--- src/main/java/com/yehor/lessons/reflectionapi/ReflectionAPIEx.java
+++ src/main/java/com/yehor/lessons/reflectionapi/ReflectionAPIEx.java
@@ -1,4 +1,11 @@
 package com.yehor.lessons.reflectionapi;
 
 public class ReflectionAPIEx {
+
+    public static void main(String[] args) {
+        User user = new User(15,"Yehor");
+        Class<? extends User> userClass = user.getClass();
+        Class<User> userClass1 = User.class;
+        System.out.println(userClass == userClass1);
+    }
 }
