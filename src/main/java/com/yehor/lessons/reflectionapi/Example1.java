af9566684bcc7849f4a2d1f45274a898a9d4e4eb
YehorYehorychev <egor57899@gmail.com>
2024-03-28 17:13:24 -0700

    Class Example1  reflectionAPI base functionality added

--- src/main/java/com/yehor/lessons/reflectionapi/Example1.java
+++ src/main/java/com/yehor/lessons/reflectionapi/Example1.java
@@ -1,8 +1,15 @@
 package com.yehor.lessons.reflectionapi;
 
+import java.lang.reflect.Field;
+
 public class Example1 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException {
+        Class employeeClass = Class.forName("com.yehor.lessons.reflectionapi.Employee");
+//        Class employeeClass2 = Employee.class;
+//        Employee em = new Employee();
+//        Class employeeClass3 = em.getClass();
+        Field id = employeeClass.getField("id");
+        System.out.println("Type of id field: " + id.getType());
     }
 }
