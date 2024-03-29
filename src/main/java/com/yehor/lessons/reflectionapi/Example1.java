17a39a200917433735c565adbe76c6c5a81b1d2f
YehorYehorychev <egor57899@gmail.com>
2024-03-28 17:27:53 -0700

    Class Example1  reflectionAPI base functionality added (Second iteration)

--- src/main/java/com/yehor/lessons/reflectionapi/Example1.java
+++ src/main/java/com/yehor/lessons/reflectionapi/Example1.java
@@ -1,6 +1,8 @@
 package com.yehor.lessons.reflectionapi;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 public class Example1 {
 
@@ -11,5 +13,14 @@ public class Example1 {
 //        Class employeeClass3 = em.getClass();
         Field id = employeeClass.getField("id");
         System.out.println("Type of id field: " + id.getType());
+        Field[] fields = employeeClass.getFields();
+        for (Field field : fields) {
+            System.out.println("Type of: " + field.getName() + " = " + field.getType());
+        }
+        System.out.println("--------------------------------------");
+        Field[] allDeclaredFields = employeeClass.getDeclaredFields();
+        for (Field field : allDeclaredFields) {
+            System.out.println("Field: " + field);
+        }
     }
 }
