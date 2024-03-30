284fc4a2599639c57923c292355cb964e268a472
YehorYehorychev <egor57899@gmail.com>
2024-03-29 19:01:45 -0700

    Class Calculator, TestCalculator reflectionAPI demo added

--- src/main/java/com/yehor/lessons/reflectionapi/TestCalculator.java
+++ src/main/java/com/yehor/lessons/reflectionapi/TestCalculator.java
@@ -1,8 +1,33 @@
 package com.yehor.lessons.reflectionapi;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class TestCalculator {
 
     public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("files/test100.txt"));) {
+            String methodName = reader.readLine();
+            String firstArgument = reader.readLine();
+            String secondArgument = reader.readLine();
 
+            Calculator calculator = new Calculator();
+            Class cl = calculator.getClass();
+            Method method = null;
+            Method[] methods = cl.getDeclaredMethods();
+            for (Method myMethod : methods) {
+                if (myMethod.getName().equals(methodName)) {
+                    method = myMethod;
+                }
+            }
+            method.invoke(calculator, Integer.parseInt(firstArgument), Integer.parseInt(secondArgument));
+
+        } catch (IOException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
