090cc9a5984d979bf3eb3bb4340426fe87bc797f
YehorYehorychev <egor57899@gmail.com>
2024-03-29 18:39:32 -0700

    Class Example2 created for the additional reflectionAPI methods demo (second iteration)

--- src/main/java/com/yehor/lessons/reflectionapi/Example2.java
+++ src/main/java/com/yehor/lessons/reflectionapi/Example2.java
@@ -1,11 +1,17 @@
 package com.yehor.lessons.reflectionapi;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Example2 {
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Class employeeClass = Class.forName("com.yehor.lessons.reflectionapi.Employee");
 
         Employee o = (Employee) employeeClass.newInstance();
         System.out.println(o);
+
+        Constructor<Employee> constructor1 = employeeClass.getConstructor();
+        Employee emp1 = constructor1.newInstance();
 
     }
 }
