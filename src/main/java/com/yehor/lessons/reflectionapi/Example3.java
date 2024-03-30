13e7515c6766af9415a85624c5a9a2de2be22814
YehorYehorychev <egor57899@gmail.com>
2024-03-29 18:47:12 -0700

    Class Example3 created for the additional reflectionAPI methods demo

---
+++ src/main/java/com/yehor/lessons/reflectionapi/Example3.java
@@ -0,0 +1,16 @@
+package com.yehor.lessons.reflectionapi;
+
+import java.lang.reflect.Field;
+
+public class Example3 {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Employee employee = new Employee(10, "Max", "IT");
+        Class employeeClass = employee.getClass();
+        Field field = employeeClass.getDeclaredField("salary");
+        field.setAccessible(true);
+
+        double salaryValue = (double) field.get(employee);
+        System.out.println(salaryValue);
+    }
+}
