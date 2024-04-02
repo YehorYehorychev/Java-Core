f70356c06f7b057a673f04b814d071d472580334
YehorYehorychev <egor57899@gmail.com>
2024-04-01 18:06:22 -0700

    Class Employee created with my custom annotation example

---
+++ src/main/java/com/yehor/lessons/annotations/Employee.java
@@ -0,0 +1,28 @@
+package com.yehor.lessons.annotations;
+
+public class Employee {
+    String name;
+    double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @MyAnnotation
+    public void increaseSalary() {
+        salary *= 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+@interface MyAnnotation {
+
+}
