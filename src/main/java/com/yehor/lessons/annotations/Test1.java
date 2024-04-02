b711203d7c9ebd929fc038009255c516038e3acb
YehorYehorychev <egor57899@gmail.com>
2024-04-01 17:58:16 -0700

    Annotation example added

--- src/main/java/com/yehor/lessons/annotations/Test1.java
+++ src/main/java/com/yehor/lessons/annotations/Test1.java
@@ -3,10 +3,32 @@ package com.yehor.lessons.annotations;
 public class Test1 {
 
     public static void main(String[] args) {
-
+        Parent p = new Child("Yehor");
+        p.showInfo();
     }
 }
 
 class Parent {
+    String name;
 
+    public Parent(String name) {
+        this.name = name;
+    }
+
+    void showInfo() {
+        System.out.println("It's Parent class. Name = " + name);
+    }
+}
+
+class Child extends Parent {
+
+    public Child(String name) {
+        super(name);
+    }
+
+    @Override
+    void showInfo() {
+        System.out.println("It's Child class. Name = " + name);
+        ;
+    }
 }
